a3f0abe0dd91a5efb6aa0f29242de8f7f40f44b7
Brian Xian <brianxian2607@gmail.com>
2023-11-02 09:11:19 -0700

    oops

---
+++ dashboard-ui/src/components/AppStats.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react'
+import '../App.css';
+
+export default function AppStats() {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [stats, setStats] = useState({});
+    const [error, setError] = useState(null)
+
+	const getStats = () => {
+	
+        fetch(`acit-3855-docker.eastus.cloudapp.azure.com:8100/stats`)
+            .then(res => res.json())
+            .then((result)=>{
+				console.log("Received Stats")
+                setStats(result);
+                setIsLoaded(true);
+            },(error) =>{
+                setError(error)
+                setIsLoaded(true);
+            })
+    }
+    useEffect(() => {
+		const interval = setInterval(() => getStats(), 2000); // Update every 2 seconds
+		return() => clearInterval(interval);
+    }, [getStats]);
+
+    if (error){
+        return (<div className={"error"}>Error found when fetching from API</div>)
+    } else if (isLoaded === false){
+        return(<div>Loading...</div>)
+    } else if (isLoaded === true){
+        return(
+            <div>
+                <h1>Latest Stats</h1>
+                <table className={"StatsTable"}>
+					<tbody>
+						<tr>
+							<th>Blood Pressure</th>
+							<th>Heart Rate</th>
+						</tr>
+						<tr>
+							<td># order_count: {stats['order_count']}</td>
+                            <td># pizza_count: {stats['pizza_count']}</td>
+						</tr>
+						<tr>
+							<td colspan="2">order_mean_cost: {stats['order_mean_cost']}</td>
+						</tr>
+						<tr>
+							<td colspan="2">order_mean_quantity: {stats['order_mean_quantity']}</td>
+						</tr>
+						<tr>
+							<td colspan="2">pizza_mean_cost: {stats['pizza_mean_cost']}</td>
+						</tr>
+                        <tr>
+                            <td colspan="2">pizza_mean_quantity: {stats['pizza_mean_quantity']}</td>
+                        </tr>
+					</tbody>
+                </table>
+                <h3>Last Updated: {stats['last_updated']}</h3>
+
+            </div>
+        )
+    }
+}
